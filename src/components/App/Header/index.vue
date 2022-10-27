77f0608c6bb83c1a40a466f9b657edaa50324b8a
1Magz1 <maxim.chipurnoff@gmail.com>
2022-10-27 15:47:23 +0600

    add header

---
+++ src/components/App/Header/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="header">
+    <h3>Test project for UPWORK</h3>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  name: "header-component",
+};
+</script>
+
+<style scoped lang="scss">
+.header {
+  padding: 24px 50px;
+  background-color: #42b983;
+  font-size: 20px;
+}
+</style>
