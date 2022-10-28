f54bebceb53c722f010dab3380ed26626544f041
1Magz1 <maxim.chipurnoff@gmail.com>
2022-10-28 16:35:15 +0600

    add home page

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,71 @@
 <template>
   <div class="home">
-    <h1>home</h1>
+    <div class="card">
+      <h2>Please login</h2>
+      <form @submit.prevent="requestLogin">
+        <BaseInput @inputValue="updateLoginInput" :options="textInputOptions" />
+        <BaseInput
+          @inputValue="updatePasswordInput"
+          :options="passwordInputOptions"
+        />
+        <button type="submit">Submit</button>
+      </form>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
-import { Vue } from "vue-class-component";
+import { defineComponent } from "vue";
+import store from "@/store";
+import BaseInput from "@/components/ui/BaseInput/index.vue";
 
-export default class HomeView extends Vue {}
+export default defineComponent({
+  name: "home-page",
+  data() {
+    return {
+      email: "",
+      password: "",
+    };
+  },
+  components: {
+    BaseInput,
+  },
+  computed: {
+    textInputOptions() {
+      return {
+        id: "input1",
+        label: "Email",
+        type: "text",
+        placeholder: "Please enter your email",
+      };
+    },
+    passwordInputOptions() {
+      return {
+        id: "input2",
+        label: "Password",
+        type: "password",
+        placeholder: "Please enter your password",
+      };
+    },
+  },
+  methods: {
+    updateLoginInput(value: string) {
+      this.email = value;
+    },
+    updatePasswordInput(value: string) {
+      this.password = value;
+    },
+    async requestLogin() {
+      const payload = {
+        login: this.email,
+        password: this.password,
+      };
+      try {
+        await store.dispatch("requestLogin", payload);
+      } catch (e) {
+        console.log("Toast error", e);
+      }
+    },
+  },
+});
 </script>
