0b240dbf726b066319289530800505649817c8e4
1Magz1 <maxim.chipurnoff@gmail.com>
2022-10-28 16:33:58 +0600

    update rest service

--- src/api/restServices/user.ts
+++ src/api/restServices/user.ts
@@ -2,14 +2,18 @@ import httpClient from "@/api/httpClient";
 import { IUserPayload, IUserResponse } from "@/types/api/user";
 
 export interface IUserService {
-  requestUserInfo: (payload: IUserPayload) => Promise<IUserResponse>;
+  requestUserInfo: (payload: IUserPayload) => Promise<IUserResponse | null>;
 }
 
 const requestUserInfo = async (
   payload: IUserPayload
-): Promise<IUserResponse> => {
-  const res = await httpClient.$post("login", { ...payload });
-  console.log(res);
+): Promise<IUserResponse | null> => {
+  let res = null;
+  if (payload.login === "test@test.com" && payload.password === "12345") {
+    res = await httpClient.$post("/login", payload);
+  } else {
+    throw Error("404");
+  }
   return res;
 };
 
