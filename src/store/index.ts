0fd1e358ee203597b03815645fda3c2f618c0197
1Magz1 <maxim.chipurnoff@gmail.com>
2022-10-28 17:29:38 +0600

    add enum for getters and setters

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,6 +1,7 @@
 import { createStore } from "vuex";
 import api from "@/api";
 import { IUserInfo } from "@/types/api/user";
+import { USER_GETTERS, USER_SETTERS } from "@/types/store";
 
 export default createStore({
   state: {
@@ -8,19 +9,32 @@ export default createStore({
     userInfo: {} as IUserInfo,
   },
   getters: {
-    getIsUserLogin: (state) => state.isUserLogin,
+    [USER_GETTERS.GET_IS_USER_LOGIN]: (state) => state.isUserLogin,
+    [USER_GETTERS.GET_USER_INFO]: (state) => state.userInfo,
   },
   mutations: {
-    SET_IS_USER_LOGIN: (state, payload: boolean) =>
+    [USER_SETTERS.SET_IS_USER_LOGIN]: (state, payload: boolean) =>
       (state.isUserLogin = payload),
-    SET_USER_INFO: (state, payload: IUserInfo) => (state.userInfo = payload),
+    [USER_SETTERS.SET_USER_INFO]: (state, payload: IUserInfo) =>
+      (state.userInfo = payload),
   },
   actions: {
-    async requestLogin(ctx, payload) {
+    async requestLogin(store, payload): Promise<boolean> {
       const res = await api.userService.requestUserInfo(payload);
       if (res) {
-        ctx.commit("SET_USER_INFO", res.data);
+        store.commit(USER_SETTERS.SET_USER_INFO, res.data);
+        store.dispatch("logIn");
+        return true;
       }
+      return false;
+    },
+    logIn(store) {
+      sessionStorage.setItem("isLogin", "true");
+      store.commit(USER_SETTERS.SET_IS_USER_LOGIN, true);
+    },
+    logOut(store) {
+      sessionStorage.setItem("isLogin", "false");
+      store.commit(USER_SETTERS.SET_IS_USER_LOGIN, false);
     },
   },
 });
