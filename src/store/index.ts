24dfb49c4c9820ae80e55e47f26a9cf754df624d
1Magz1 <maxim.chipurnoff@gmail.com>
2022-10-28 16:34:17 +0600

    add store

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,9 +1,26 @@
 import { createStore } from "vuex";
+import api from "@/api";
+import { IUserInfo } from "@/types/api/user";
 
 export default createStore({
-  state: {},
-  getters: {},
-  mutations: {},
-  actions: {},
-  modules: {},
+  state: {
+    isUserLogin: false,
+    userInfo: {} as IUserInfo,
+  },
+  getters: {
+    getIsUserLogin: (state) => state.isUserLogin,
+  },
+  mutations: {
+    SET_IS_USER_LOGIN: (state, payload: boolean) =>
+      (state.isUserLogin = payload),
+    SET_USER_INFO: (state, payload: IUserInfo) => (state.userInfo = payload),
+  },
+  actions: {
+    async requestLogin(ctx, payload) {
+      const res = await api.userService.requestUserInfo(payload);
+      if (res) {
+        ctx.commit("SET_USER_INFO", res.data);
+      }
+    },
+  },
 });
