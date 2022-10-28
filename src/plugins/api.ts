7484446a79dd661c11bd421f19d90a2865ee574a
1Magz1 <maxim.chipurnoff@gmail.com>
2022-10-28 16:35:00 +0600

    update base url

--- src/plugins/api.ts
+++ src/plugins/api.ts
@@ -3,7 +3,9 @@ import api, { IApiService, setHTTPClientInstance } from "@/api";
 
 const httpServicePlugin = {
   install(Vue: any) {
-    const axiosInstance = axios.create();
+    const axiosInstance = axios.create({
+      baseURL: "https://e4514330-641e-4461-9363-abecdca5acdd.mock.pstmn.io",
+    });
     setHTTPClientInstance({ ...axiosInstance });
     Vue.config.globalProperties.$api = api;
   },
